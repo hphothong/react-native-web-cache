d0832fd74a18ae6b7ccdfb80704cc80ece691b09
hphothong <hphothong@gmail.com>
2020-11-24 18:53:39 -0700

    Remove unused interface serializable

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,4 +2,3 @@ export { ICache } from "./ICache";
 export { ICacheOptions } from "./ICacheOptions";
 export { ICacheStore } from "./ICacheStore";
 export { ICacheEntry } from "./ICacheEntry";
-export { ISerializable } from "./ISerializable";
